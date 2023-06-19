17489134a965e9a50b5f3b999fe61fa3273e7fc5
Bernardo Luiz Bach <bl.bach@hotmail.com>
2023-06-19 01:38:06 -0300

    .

---
+++ Workspace/Ludo-INF1636/src/view/Peca.java
@@ -0,0 +1,29 @@
+package view;
+
+class Peca {
+	private int cor;
+	private int id_peca;
+	private int pos_x, pos_y;
+	
+	
+	protected void Peca(int id_time, int cor, int x, int y) {
+		
+	}
+	
+	protected void draw_peca(int index, int list_index) {
+		
+	}
+	
+	protected void draw_abrigo_stack(int cor1, int cor2, int pos1, int pos2) {
+		
+	}
+	
+	protected void draw_barreira(int id, int pos1, int pos2) {
+		
+	}
+	
+	protected void draw_finish(int id) {
+		
+	}
+	
+}

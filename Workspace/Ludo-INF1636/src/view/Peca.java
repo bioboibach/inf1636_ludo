745edd0e6bd00723f3925f5b65a5f097bc449b80
LucasToscanoPAC <lucas.toscano.pac@gmail.com>
2023-06-19 05:48:13 -0300

    bbb

--- Workspace/Ludo-INF1636/src/view/Peca.java
+++ Workspace/Ludo-INF1636/src/view/Peca.java
@@ -4,48 +4,33 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 
-class Peca {
+public class Peca {
 	private int id;		// Indice da peca pro player 
 	private int cor;
 	private int x;
 	private int y;
 	
-
-	protected void start_arr_coordInicial() {
-		//	Vermelho
-		coordInicial[0] = new int[] {57, 57};
-		coordInicial[1] = new int[] {197, 57};
-		coordInicial[2] = new int[] {57, 177};
-		coordInicial[3] = new int[] {197, 177};
-			
-		//	Verde
-		coordInicial[4] = new int[] {492, 57};
-		coordInicial[5] = new int[] {632, 57};
-		coordInicial[6] = new int[] {632, 177};
-		coordInicial[7] = new int[] {492, 177};
-			
-		//	Azul
-		coordInicial[8] = new int[] {57, 492};
-		coordInicial[9] = new int[] {197, 492};
-		coordInicial[10] = new int[] {57, 612};
-		coordInicial[11] = new int[] {197, 612};
-			
-		//	Amarelo
-		coordInicial[12] = new int[] {632, 312};
-		coordInicial[13] = new int[] {632, 492};
-		coordInicial[14] = new int[] {492, 492};
-		coordInicial[15] = new int[] {492, 612};
-	}
+//	Posicionamento
+	private static final int BOARD_SIZE = 720;			// Tamanho do lado do tabuleiro
+	private static final int SIZE = BOARD_SIZE / 15; 	// Tamanho de cada célula do tabuleiro
 	
-	protected int[][] coordInicial1 = new int[16][2];		// Coordenadas das casas iniciais
+	// Coordenadas do tabuleiros convertidas para a casa correspondente
+	int[][] coordsMapeadas = new int[52][2];				
+	int[][] coordsMapeadas_vermelho = new int[6][2];		
+	int[][] coordsMapeadas_azul = new int[6][2];			
+	int[][] coordsMapeadas_amarelo = new int[6][2];			
+	int[][] coordsMapeadas_verde = new int[6][2];		
 	
-	private int[][] coordInicial = LudoBoard.getInstance().get_coordInicial();		// Coordenadas de todas as casas iniciais em ordem dos jogadores
+	// Coordenadas do tabuleiros convertidas para a coordenada da casa correspondente
+	int[][] coordsMapeadasDesenho = new int[52][2];			
+	int[][] coordsMapeadasDesenho_vermelho = new int[6][2];	
+	int[][] coordsMapeadasDesenho_azul = new int[6][2];		
+	int[][] coordsMapeadasDesenho_amarelo = new int[6][2];	
+	int[][] coordsMapeadasDesenho_verde = new int[6][2];	
 	
-	private int[][] coordsMapeadasDesenho			= LudoBoard.getInstance().get_coordsMapeadasDesenho();
-	private int[][] coordsMapeadasDesenho_vermelho 	= LudoBoard.getInstance().get_coordsMapeadasDesenho_vermelho();
-	private int[][] coordsMapeadasDesenho_azul		= LudoBoard.getInstance().get_coordsMapeadasDesenho_azul();
-	private int[][] coordsMapeadasDesenho_amarelo 	= LudoBoard.getInstance().get_coordsMapeadasDesenho_amarelo();
-	private int[][] coordsMapeadasDesenho_verde 	= LudoBoard.getInstance().get_coordsMapeadasDesenho_verde();
+	protected int[][] coordInicial = new int[16][2];		// Coordenadas de todas as casas iniciais em ordem dos jogadores
+	
+	
 	
 	private static final Color[] COLORS = { Color.RED, Color.GREEN, Color.YELLOW, Color.BLUE, Color.WHITE, Color.BLACK };
 
@@ -55,24 +40,19 @@ class Peca {
 		this.cor = cor;
 		x = coordInicial[4*cor + id][0];
 		y = coordInicial[4*cor + id][1];
+		start_arr_coordInicial();
+		start_path_mapeado(); 		// Inicializa as casas do path
+		start_final_path_mapeado(); // Inicializa as casas do final_path
 		
 	}
 	
 	protected void draw_inicio(Graphics g) {
-//		int x = coordInicial[4*cor + id][0];
-//		int y = coordInicial[4*cor + id][1];
-		x = coordInicial1[0][0];
-		y = coordInicial1[0][1];
+		int x = coordInicial[4*cor + id][0];
+		int y = coordInicial[4*cor + id][1];
 		
-		System.out.println("CHECK --> ");
-		System.out.println(x);
-		System.out.println(y);
-		
-		x = 10;
-		y = 10;
 		g.setColor(Color.BLACK);
 		g.drawOval(x, y, 25, 25);
-		g.setColor(Color.BLUE);
+		g.setColor(COLORS[cor]);
 		g.fillOval(x, y, 25, 25);
 	}
 
@@ -85,6 +65,8 @@ class Peca {
 		g.drawOval(x, y, 25, 25);
 		g.setColor(COLORS[cor]);
 		g.fillOval(x, y, 25, 25);
+		
+//		run_repaint();
 		
 	}
 	
@@ -152,6 +134,134 @@ class Peca {
 	}
 	
 	
+//	Inicializadores ------------------------------
+	protected void start_arr_coordInicial() {
+		//	Vermelho
+		coordInicial[0] = new int[] {57, 57};
+		coordInicial[1] = new int[] {197, 57};
+		coordInicial[2] = new int[] {57, 177};
+		coordInicial[3] = new int[] {197, 177};
+			
+		//	Verde
+		coordInicial[4] = new int[] {492, 57};
+		coordInicial[5] = new int[] {632, 57};
+		coordInicial[6] = new int[] {632, 177};
+		coordInicial[7] = new int[] {492, 177};
+			
+		//	Azul
+		coordInicial[12] = new int[] {57, 492};
+		coordInicial[13] = new int[] {197, 492};
+		coordInicial[14] = new int[] {57, 612};
+		coordInicial[15] = new int[] {197, 612};
+		
+			
+		//	Amarelo
+		coordInicial[8] = new int[] {632, 612};
+		coordInicial[9] = new int[] {632, 492};
+		coordInicial[10] = new int[] {492, 492};
+		coordInicial[11] = new int[] {492, 612};
+	}
+	protected void start_path_mapeado() {
+		coordsMapeadas[0] = new int[] {1, 6};
+		coordsMapeadas[1] = new int[] {0, 6};
+		coordsMapeadas[2] = new int[] {0, 7};
+		coordsMapeadas[3] = new int[] {0, 8};
+		coordsMapeadas[4] = new int[] {1, 8};
+		coordsMapeadas[5] = new int[] {2, 8};
+		coordsMapeadas[6] = new int[] {3, 8};
+		coordsMapeadas[7] = new int[] {4, 8};
+		coordsMapeadas[8] = new int[] {5, 8};
+		coordsMapeadas[9] = new int[] {6, 9};
+		coordsMapeadas[10] = new int[] {6, 10};
+		coordsMapeadas[11] = new int[] {6, 11};
+		coordsMapeadas[12] = new int[] {6, 12};
+		coordsMapeadas[13] = new int[] {6, 13};
+		coordsMapeadas[14] = new int[] {6, 14};
+		coordsMapeadas[15] = new int[] {7, 14};
+		coordsMapeadas[16] = new int[] {8, 14};
+		coordsMapeadas[17] = new int[] {8, 13};
+		coordsMapeadas[18] = new int[] {8, 12};
+		coordsMapeadas[19] = new int[] {8, 11};
+		coordsMapeadas[20] = new int[] {8, 10};
+		coordsMapeadas[21] = new int[] {8, 9};
+		coordsMapeadas[22] = new int[] {9, 8};
+		coordsMapeadas[23] = new int[] {10, 8};
+		coordsMapeadas[24] = new int[] {11, 8};
+		coordsMapeadas[25] = new int[] {12, 8};
+		coordsMapeadas[26] = new int[] {13, 8};
+		coordsMapeadas[27] = new int[] {14, 8};
+		coordsMapeadas[28] = new int[] {14, 7};
+		coordsMapeadas[29] = new int[] {14, 6};
+		coordsMapeadas[30] = new int[] {13, 6};
+		coordsMapeadas[31] = new int[] {12, 6};
+		coordsMapeadas[32] = new int[] {11, 6};
+		coordsMapeadas[33] = new int[] {10, 6};
+		coordsMapeadas[34] = new int[] {9, 6};
+		coordsMapeadas[35] = new int[] {8, 5};
+		coordsMapeadas[36] = new int[] {8, 4};
+		coordsMapeadas[37] = new int[] {8, 3};
+		coordsMapeadas[38] = new int[] {8, 2};
+		coordsMapeadas[39] = new int[] {8, 1};
+		coordsMapeadas[40] = new int[] {8, 0};
+		coordsMapeadas[41] = new int[] {7, 0};
+		coordsMapeadas[42] = new int[] {6, 0};
+		coordsMapeadas[43] = new int[] {6, 1};
+		coordsMapeadas[44] = new int[] {6, 2};
+		coordsMapeadas[45] = new int[] {6, 3};
+		coordsMapeadas[46] = new int[] {6, 4};
+		coordsMapeadas[47] = new int[] {6, 5};
+		coordsMapeadas[48] = new int[] {5, 6};
+		coordsMapeadas[49] = new int[] {4, 6};
+		coordsMapeadas[50] = new int[] {3, 6};
+		coordsMapeadas[51] = new int[] {2, 6};
+		
+		
+		
+		for(int i = 0; i < 52; i++) {
+			coordsMapeadasDesenho[i][0] += coordsMapeadas[i][0]*SIZE + 12;
+			coordsMapeadasDesenho[i][1] += coordsMapeadas[i][1]*SIZE + 12;
+		}
+	}
+	protected void start_final_path_mapeado() {
+		coordsMapeadas_vermelho[0] = new int[] {1, 7};
+		coordsMapeadas_vermelho[1] = new int[] {2, 7};
+		coordsMapeadas_vermelho[2] = new int[] {3, 7};
+		coordsMapeadas_vermelho[3] = new int[] {4, 7};
+		coordsMapeadas_vermelho[4] = new int[] {5, 7};
+		coordsMapeadas_vermelho[5] = new int[] {6, 7};
+		
+		coordsMapeadas_azul[0] = new int[] {7, 13};
+		coordsMapeadas_azul[1] = new int[] {7, 12};
+		coordsMapeadas_azul[2] = new int[] {7, 11};
+		coordsMapeadas_azul[3] = new int[] {7, 10};
+		coordsMapeadas_azul[4] = new int[] {7, 9};
+		coordsMapeadas_azul[5] = new int[] {7, 8};
+		
+		coordsMapeadas_amarelo[0] = new int[] {13, 7};
+		coordsMapeadas_amarelo[1] = new int[] {12, 7};
+		coordsMapeadas_amarelo[2] = new int[] {11, 7};
+		coordsMapeadas_amarelo[3] = new int[] {10, 7};
+		coordsMapeadas_amarelo[4] = new int[] {9, 7};
+		coordsMapeadas_amarelo[5] = new int[] {8, 7};
+		
+		coordsMapeadas_verde[0] = new int[] {7, 1};
+		coordsMapeadas_verde[1] = new int[] {7, 2};
+		coordsMapeadas_verde[2] = new int[] {7, 3};
+		coordsMapeadas_verde[3] = new int[] {7, 4};
+		coordsMapeadas_verde[4] = new int[] {7, 5};
+		coordsMapeadas_verde[5] = new int[] {7, 6};
+		
+		for(int i = 0; i < 6; i++) {
+			coordsMapeadasDesenho_vermelho[i][0] += coordsMapeadas_vermelho[i][0]*SIZE + 12;
+			coordsMapeadasDesenho_vermelho[i][1] += coordsMapeadas_vermelho[i][1]*SIZE + 12;
+			coordsMapeadasDesenho_azul[i][0] += coordsMapeadas_azul[i][0]*SIZE + 12;
+			coordsMapeadasDesenho_azul[i][1] += coordsMapeadas_azul[i][1]*SIZE + 12;
+			coordsMapeadasDesenho_amarelo[i][0] += coordsMapeadas_amarelo[i][0]*SIZE + 12;
+			coordsMapeadasDesenho_amarelo[i][1] += coordsMapeadas_amarelo[i][1]*SIZE + 12;
+			coordsMapeadasDesenho_verde[i][0] += coordsMapeadas_verde[i][0]*SIZE + 12;
+			coordsMapeadasDesenho_verde[i][1] += coordsMapeadas_verde[i][1]*SIZE + 12;
+		}
+	}
 	protected int get_cor() {
 		return cor;
 	}

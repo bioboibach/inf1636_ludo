3e0d035bfdba6b0b611da0cc665cd8ae7507616a
Guilherme Ponce <guilhermeponce47@gmail.com>
2023-07-06 13:55:53 -0300

    Create ModelAPI.java

---
+++ Workspace/Ludo-INF1636/src/modal/ModelAPI.java
@@ -0,0 +1,8 @@
+package modal;
+
+public class ModelAPI {
+
+	//TODO
+	// Colocar nessa API todas as funçoes que os outros modulos irao precisar
+	// Exemplos -> Roll do dado, get e set posicao das pecas, isBarrier, isAbrigo etc 
+}
